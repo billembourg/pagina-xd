4e2177dbcf18870e14e6be84da0e84025360d48b
Bill <Billembourg@gmail.com>
2023-12-19 22:15:25 -0400

    ultiam actualizacion martes

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,35 @@
-var imagenes = ['img/img1.jpg', 'img/img2.jpg', 'img/img3.jpg'];
+var imagenes = ['img/img1.png', 'img/img2.jpg', 'img/img3.jpg'],
     cont=0;
 
 function carrusel(slider){
-    slider.add
+    slider.addEventListener('click', e =>{
+        let atras = slider.querySelector('.atras'),
+            adelante = slider.querySelector('.adelante'),
+            img = slider.querySelector('img'),
+            tgt = e.target;
+
+            if(tgt==atras){
+                if(cont > 0){
+                    img.src = imagenes[cont - 1];
+                    cont--;
+                }else{
+                    img.src = imagenes[imagenes.length - 1];
+                    cont = imagenes.length - 1;
+                }
+
+            }else if(tgt == adelante){
+                if(cont < imagenes.length - 1){
+                    img.src = imagenes[cont + 1];
+                    cont++;
+                }else{
+                    img.src = imagenes[0];
+                    cont = 0;
+                }
+            }           
+    });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    let slider = document.querySelector('.slider');
+    carrusel(slider);
+});
